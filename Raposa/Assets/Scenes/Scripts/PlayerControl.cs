155b58dc14abf8e284b62f9781533d160b9c1aa6
LimaXII <luccasdasilvalima@hotmail.com>
2022-04-12 21:03:40 -0300

    Character and collision

---
+++ Raposa/Assets/Scenes/Scripts/PlayerControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
