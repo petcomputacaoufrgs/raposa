4580fbdad99837bae3ed246643e7cfa4d6a48c13
Matheus <matheus.almeidasil@outlook.com>
2022-04-13 12:11:58 -0300

    Revert "Movement and camera"
    This reverts commit f43b9c1e0ea18baafa2dab62af0e7e0ad62b93c8.

--- Raposa/Assets/Scenes/Scripts/PlayerControl.cs
+++ Raposa/Assets/Scenes/Scripts/PlayerControl.cs
@@ -4,28 +4,9 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float Speed = 1.5f;
-    private Rigidbody rb;
-    private Vector2 direction;
-
-    private void Start()
+    // Update is called once per frame
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
-
-        direction = new Vector2(xAxis, yAxis);
         
-    }
-
-    private void FixedUpdate()
-    {
-        GetComponent<Rigidbody2D>().MovePosition
-            (GetComponent<Rigidbody2D>().position +
-            (direction * Speed * Time.deltaTime));
     }
 }
