f43b9c1e0ea18baafa2dab62af0e7e0ad62b93c8
HenriqueCarniel <henriquecarniel80@gmail.com>
2022-04-13 12:05:15 -0300

    Movement and camera

--- Raposa/Assets/Scenes/Scripts/PlayerControl.cs
+++ Raposa/Assets/Scenes/Scripts/PlayerControl.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    public float Speed = 1.5f;
+    private Rigidbody rb;
+    private Vector2 direction;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        direction = new Vector2(xAxis, yAxis);
         
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().MovePosition
+            (GetComponent<Rigidbody2D>().position +
+            (direction * Speed * Time.deltaTime));
     }
 }
