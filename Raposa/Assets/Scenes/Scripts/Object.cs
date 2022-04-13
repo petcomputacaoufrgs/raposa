79b55dd30192aed25da443f0215da17af1e3ef86
LimaXII <luccasdasilvalima@hotmail.com>
2022-04-13 15:37:42 -0300

    Interaction with the environment

---
+++ Raposa/Assets/Scenes/Scripts/Object.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public float dist = 0.2f;    
+    public GameObject Player;    
+    Vector2 distanceObject; 
+
+    // Update is called once per frame
+    void Update()    
+    {
+        distanceObject = transform.position - Player.transform.position;
+        if (distanceObject.magnitude <= dist)
+        {            
+            Debug.Log("Distance:" + distanceObject.magnitude);
+            if(Input.GetButtonDown("Fire1"))
+            {                
+                Destroy(gameObject);
+            }           
+        }
+    }
+}
