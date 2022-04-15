e12cd62a6fc2f6af165134d1c8bd8011857d2bc4
HenriqueCarniel <henriquecarniel80@gmail.com>
2022-04-15 11:58:26 -0300

    Movement animations

--- Raposa/Assets/Scenes/Scripts/PlayerControl.cs
+++ Raposa/Assets/Scenes/Scripts/PlayerControl.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float Speed = 1.5f;
-    private Rigidbody rb;
+    public float Speed = 0.7f;
+    private Rigidbody2D rb;
     private Vector2 direction;
+    private Animator animator;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
 
         direction = new Vector2(xAxis, yAxis);
 
+        animator.SetFloat("horizontal", direction.x);
+        animator.SetFloat("vertical", direction.y);
+        animator.SetFloat("speed", direction.sqrMagnitude);
+
+        if (direction != Vector2.zero)
+        {
+            animator.SetFloat("horizontalIdle", direction.x);
+            animator.SetFloat("verticalIdle", direction.y);
+        }
     }
 
     private void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().MovePosition
-            (GetComponent<Rigidbody2D>().position +
-            (direction * Speed * Time.deltaTime));
+        rb.MovePosition
+            (rb.position +
+            (direction.normalized * Speed * Time.fixedDeltaTime));
     }
 }
