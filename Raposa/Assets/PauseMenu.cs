98ff791425473630857ead9664f4f4a8b6465522
HenriqueCarniel <henriquecarniel80@gmail.com>
2022-04-20 18:30:14 -0300

    Input key "M"

--- Raposa/Assets/PauseMenu.cs
+++ Raposa/Assets/PauseMenu.cs
@@ -13,7 +13,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.M))
         {
             if (GameIsPaused)
             {
