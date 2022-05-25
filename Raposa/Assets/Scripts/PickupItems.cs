8582c3f80a3a1ac7ae6344417469b86b75a863fb
Ian Kersz <kerszamaral@gmail.com>
2022-05-25 16:25:40 -0300

    PickupItems game logic done

---
+++ Raposa/Assets/Scripts/PickupItems.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItems : MonoBehaviour
+{
+    public float dist = 0.2f;
+    public GameObject Player;
+    Vector2 distanceObject;
+    private InGameUI inGameUI;
+
+    private void Start()
+    {
+        inGameUI = FindObjectOfType(typeof(InGameUI)) as InGameUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PauseMenu.GameIsPaused)
+        {
+            distanceObject = transform.position - Player.transform.position;
+            if (distanceObject.magnitude <= dist)
+            {
+                Debug.Log("Distance:" + distanceObject.magnitude);
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    Destroy(gameObject);
+
+                    inGameUI.ReloadDiamondsText();
+
+                    Debug.Log("Number of diamonds: " + inGameUI.NumDiamonds);
+                }
+            }
+        }
+    }
+}
