1100aa59754f572bbd97f552f7e3344eee6f60c2
Ian Kersz <kerszamaral@gmail.com>
2022-06-30 19:24:48 -0300

    Renamed variables and explicited access modifiers

--- Raposa/Assets/Scripts/CameraControl.cs
+++ Raposa/Assets/Scripts/CameraControl.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public GameObject Player;
-    Vector3 distanceCamera;
+    public Vector3 DistanceCamera;
 
     private void Start()
     {
-        distanceCamera = transform.position - Player.transform.position;
+        DistanceCamera = transform.position - Player.transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.position = Player.transform.position + distanceCamera;
+        transform.position = Player.transform.position + DistanceCamera;
     }
 }
