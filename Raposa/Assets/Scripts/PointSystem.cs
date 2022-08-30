281db9a56198890a36e232f764fb98312ae42123
Ian Kersz <kerszamaral@gmail.com>
2022-08-30 01:26:51 -0300

    Updated method to get points

--- Raposa/Assets/Scripts/PointSystem.cs
+++ Raposa/Assets/Scripts/PointSystem.cs
@@ -5,30 +5,26 @@ using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour
 {
-    private static int points = 0;
+    public static int Points {get;private set;}= 0;
     private static Text textPoints;
     private void Start()
     {
-        points = 0;
+        Points = 0;
         textPoints = GameObject.FindWithTag("PointTextLocation").GetComponent<Text>();
         UpdateText();
     }
     public static void AddPoints(int amount)
     {
-        points += amount;
+        Points += amount;
         UpdateText();
     }
     public static void RemovePoints(int amount)
     {
-        points -= amount;
+        Points -= amount;
         UpdateText();
-    }
-    public static int GetPoints()
-    {
-        return points;
     }
     private static void UpdateText()
     {
-        textPoints.text = "Points: " + points.ToString();
+        textPoints.text = "Points: " + Points.ToString();
     }
 }
