77d2a6a56f3d7f37957b398161a8a62acfc9ab0c
Ian Kersz <kerszamaral@gmail.com>
2022-11-02 23:06:58 -0300

    Fixed script no compiling on build
    Co-authored-by: HenriqueCarniel <HenriqueCarniel@users.noreply.github.com>

--- Raposa/Assets/Scripts/NPCSpawn.cs
+++ Raposa/Assets/Scripts/NPCSpawn.cs
@@ -44,12 +44,23 @@ public class NPCSpawn : MonoBehaviour
         {
             if (Order.GetType() != typeof(Transform))
             {
-                UnityEditorInternal.ComponentUtility.CopyComponent(Order);
-                UnityEditorInternal.ComponentUtility.PasteComponentAsNew(NPC);
+                CopyComponent<Component>(Order, NPC);
             }
         }
 
         return NPC;
+    }
+
+    T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        System.Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(original));
+        }
+        return copy as T;
     }
 }
 
